c23549617cbd0619ceeb46b1e3daa6af8134233f
Amit Hemo <itsamit442@gmail.com>
2024-09-06 16:56:19 +0300

    refactor: Bind balance data to balance microservice

--- libs/shared/src/file-management/file-management.service.ts
+++ libs/shared/src/file-management/file-management.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import { discoverServicePath } from '../utils/discoverServicePath';
 
 @Injectable()
 export class FileManagementService {
-  private readonly dataPath = path.join(process.cwd(), 'data');
+  private readonly DATA_DIR_PATH = path.join(process.cwd(), 'data'); //default
 
   constructor() {
-    fs.mkdir(this.dataPath, { recursive: true }).catch((err) => {
+    this.DATA_DIR_PATH = path.join(discoverServicePath(__dirname), 'data');
+    fs.mkdir(this.DATA_DIR_PATH, { recursive: true }).catch((err) => {
       console.error('Error creating data directory:', err);
     });
   }
 
   async readJSON<T extends object>(filename: string): Promise<T | null> {
-    const filePath = path.join(this.dataPath, `${filename}.json`);
+    const filePath = path.join(this.DATA_DIR_PATH, `${filename}.json`);
     try {
       const data = await fs.readFile(filePath, 'utf-8');
       return JSON.parse(data) as T;
@@ -28,7 +30,7 @@ export class FileManagementService {
   }
 
   async writeJSON<T extends object>(filename: string, data: T): Promise<void> {
-    const filePath = path.join(this.dataPath, `${filename}.json`);
+    const filePath = path.join(this.DATA_DIR_PATH, `${filename}.json`);
     try {
       await fs.writeFile(filePath, JSON.stringify(data), 'utf-8');
       console.log(`File written: ${filePath}`);
